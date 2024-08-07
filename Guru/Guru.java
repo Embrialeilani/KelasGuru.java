6b9dc25c991e61d2008d6a4e4e8a8ab8797093d2
Embrialeilani <embria_shanawa_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:49:52 +0700

    Add files via upload

---
+++ Guru/Guru.java
@@ -0,0 +1,17 @@
+//Object Class
+public class Guru {
+    String guru;
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    /*Constructor*/
+    public Guru() {
+        id = 5;
+        nama = "Aulia, Diaur, Pasha, Andre, Feni ";
+        mapel = "Produktif RPL, Ipas, Produktif RPL, Bahasa Inggris, Matematika";
+        alamat = "Malang, Malang, Malang, Malang, Malang  ";
+
+    }
+}
---
+++ Guru/TesGuru.java
@@ -0,0 +1,57 @@
+//Driver  Class
+public class TesGuru {
+    public static void main(String[] args) {
+        //membuat obyek
+        Guru aulia = new Guru();
+        Guru diaur = new Guru();
+        Guru pasha = new Guru();
+        Guru andre = new Guru();
+        Guru feni = new Guru();
+        aulia.id = 1;
+        aulia.nama = "Aulia";
+        aulia.mapel = "Produktif RPL";
+        aulia.alamat = "Malang";
+        diaur.id = 2;
+        diaur.nama = "Diaur";
+        diaur.mapel = "Ipas";
+        diaur.alamat = "Malang";
+        pasha.id = 3;
+        pasha.nama = "Pasha";
+        pasha.mapel = "Produktif RPL";
+        pasha.alamat = "Malang";
+        andre.id = 4;
+        andre.nama = "Andre";
+        andre.mapel = "Bahasa Inggris";
+        andre.alamat = "Malang";
+        feni.id = 5;
+        feni.nama = "Feni";
+        feni.mapel = "Matematika";
+        feni.alamat = "Malang";
+        System.out.println("Ini Data Aulia");
+        System.out.println(aulia.id);
+        System.out.println(aulia.nama);
+        System.out.println(aulia.mapel);
+        System.out.println(aulia.alamat);
+        System.out.println("Ini Data Diaur");
+        System.out.println(diaur.id);
+        System.out.println(diaur.nama);
+        System.out.println(diaur.mapel);
+        System.out.println(diaur.alamat);
+        System.out.println("Ini Data Pasha");
+        System.out.println(pasha.id);
+        System.out.println(pasha.nama);
+        System.out.println(pasha.mapel);
+        System.out.println(pasha.alamat);
+        System.out.println("Ini Data Andre;");
+        System.out.println(andre.id);
+        System.out.println(andre.nama);
+        System.out.println(andre.mapel);
+        System.out.println(andre.alamat);
+        System.out.println("Ini Data Feni");
+        System.out.println(feni.id);
+        System.out.println(feni.nama);
+        System.out.println(feni.mapel);
+        System.out.println(feni.alamat);
+
+    }
+}
